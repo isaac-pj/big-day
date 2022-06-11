c596e3f4a512b78ae70c3bce621cc9a5946aba4f
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-11 11:50:17 -0300

    feat(components): add footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.style.scss'
+
+const Footer = () => {
+  return ( <div className="Footer">
+    <span>Made by</span> <span className="Footer__strong">Crown Studio © 2022</span>
+  </div> );
+}
+ 
+export default Footer;
