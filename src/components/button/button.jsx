24ab426ff063a3c7fdbb463e94df307d3f7fd69f
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-15 02:43:28 -0300

    feat(components): some changes for button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,12 +1,20 @@
 import classNames from "classnames";
-import React from "react";
+import React, { memo } from "react";
 import { BUTTON_TYPES } from "../../constants/general";
 import Icon from "../icon/Icon";
 import "./button.style.scss";
 
-const Button = ({ type = BUTTON_TYPES.RAISED, text = "button", icon }) => {
+const Button = ({
+  type = BUTTON_TYPES.RAISED,
+  text = "button",
+  icon,
+  onClick,
+}) => {
   return (
-    <button className={classNames("Button", type, icon && "ICON")}>
+    <button
+      onClick={onClick}
+      className={classNames("Button", type, icon && "ICON")}
+    >
       {icon && <Icon name={icon} />}
       {text && type != BUTTON_TYPES.FAB && (
         <span className="Button__text">{text}</span>
@@ -15,4 +23,4 @@ const Button = ({ type = BUTTON_TYPES.RAISED, text = "button", icon }) => {
   );
 };
 
-export default Button;
+export default memo(Button);
