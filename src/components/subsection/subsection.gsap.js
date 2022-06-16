0698da37ce7f5c44a45645a7198a5207530c203c
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-16 05:13:17 -0300

    feat(section): animation for subsection elements

---
+++ src/components/subsection/subsection.gsap.js
@@ -0,0 +1,32 @@
+const scaleUp = (gsap, elem, trigger) => {
+  gsap.fromTo(
+    elem,
+    { scale: 0, opacity: 0 },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 1.5,
+      scrollTrigger: {
+        trigger: trigger || elem,
+      },
+    },
+  )
+}
+
+const slideIn = (gsap, elem, trigger, { reverse }) => {
+  gsap.fromTo(
+    elem,
+    { translateX: reverse ? -800 : 800, opacity: 0 },
+    {
+      translateX: 0,
+      opacity: 1,
+      duration: 1.5,
+      scrollTrigger: {
+        trigger: trigger || elem,
+        // start: 'top',
+      },
+    },
+  )
+}
+
+export { scaleUp, slideIn }
--- src/components/subsection/subsection.jsx
+++ src/components/subsection/subsection.jsx
@@ -1,13 +1,54 @@
-import classNames from "classnames";
-import React from "react";
-import Button from "../button/button";
+import React, { useRef, useEffect } from "react";
 import "./subsection.style.scss";
+import classNames from "classnames";
+import Button from "../button/button";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import Toast from "../toast/toast";
+import * as animations from "./subsection.gsap";
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-const Subsection = ({ title, image, description, alert, colors }) => {
+const Subsection = ({ title, image, description, alert, colors, reverse }) => {
+  const toastRef = useRef(null);
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+  const infoRef = useRef(null);
+
+  useEffect(() => {
+    const imageElem = imageRef.current;
+    const infoElem = infoRef.current;
+    const containerElem = containerRef.current;
+    const reverse = JSON.parse(infoElem?.dataset?.reverse);
+
+    animations.scaleUp(gsap, imageElem, containerElem);
+    animations.slideIn(gsap, infoElem, containerElem, { reverse });
+  }, []);
+
+  const handleCopyColor = (event, { name, hex, cmyk }) => {
+    const color = `${name}: HEX: (${hex}) CMYK: (${cmyk})`;
+    navigator.clipboard.writeText(color);
+    toastRef.current.showToast();
+  };
+
+  const handlePrintPallete = () => {
+    gsap.to(window, { duration: 2, scrollTo: "#colors" });
+  };
+
   return (
-    <div className="Subsection" title={title}>
-      <img className="Subsection__image" src={image} alt={title} />
-      <div className="Subsection__info">
+    <div
+      ref={containerRef}
+      className="Subsection"
+      title={title}
+      style={{ flexDirection: reverse ? "row-reverse" : "row" }}
+    >
+      <img
+        ref={imageRef}
+        className="Subsection__image"
+        src={image}
+        alt={title}
+      />
+      <div ref={infoRef} data-reverse={reverse} className="Subsection__info">
         <div>
           <h1 className="Subsection__title">{title}</h1>
           <h5 className="Subsection__description">{description}</h5>
@@ -21,10 +62,18 @@ const Subsection = ({ title, image, description, alert, colors }) => {
           </div>
         </div>
         <div className="Subsection__buttons">
-          <Button text="Copiar Cor" />
-          <Button text="Imprimir Paleta" />
+          <Button
+            text="Copiar Cor"
+            onClick={(e) => handleCopyColor(e, colors)}
+          />
+          <Button text="Imprimir Paleta" onClick={handlePrintPallete} />
         </div>
       </div>
+      <Toast
+        ref={toastRef}
+        message=":) A cor foi copiada com sucesso!"
+        timeout={3000}
+      />
     </div>
   );
 };
