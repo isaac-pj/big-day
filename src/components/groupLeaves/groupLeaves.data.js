ad113b114de216f7a0bd73eb93e033d11f098fce
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-17 03:30:29 -0300

    feat(components): add group leaves

---
+++ src/components/groupLeaves/groupLeaves.data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 'leaf01',
+    position: {
+      desktop: {
+        top: -250,
+        right: -300,
+        bottom: 'unset',
+        left: 'unset',
+      },
+      mobile: {
+        top: -100,
+        right: -100,
+        bottom: 'unset',
+        left: 'unset',
+      },
+    },
+    reverse: false,
+    scrub: 1,
+    transform: {
+      x: 1200,
+      y: -3000,
+    },
+    url: null,
+  },
+]
---
+++ src/components/groupLeaves/groupLeaves.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import "./groupLeaves.style.scss";
+import dataLeaves from "./groupLeaves.data";
+import FloatLeaf from "../floatLeaf/floatLeaf";
+
+const GroupLeaves = () => {
+  const _renderLeaves = (leaves) => {
+    return leaves.map(({ id, ...data }) => {
+      return <FloatLeaf key={id} id={id} {...data} />;
+    });
+  };
+
+  return <div className="GroupLeaves">{_renderLeaves(dataLeaves)}</div>;
+};
+
+export default GroupLeaves;
