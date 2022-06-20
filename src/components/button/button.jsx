57c682c8ebe1abe19051ddbb4f8fafaf5c339b01
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-20 20:28:07 -0300

    feat(fab-button): pin fab button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,17 +1,16 @@
 import classNames from "classnames";
-import React, { memo } from "react";
+import React, { forwardRef, memo } from "react";
 import { BUTTON_TYPES } from "../../constants/general";
 import Icon from "../icon/Icon";
 import "./button.style.scss";
 
-const Button = ({
-  type = BUTTON_TYPES.RAISED,
-  text = "button",
-  icon,
-  onClick,
-}) => {
+const Button = (
+  { type = BUTTON_TYPES.RAISED, text = "button", icon, onClick },
+  ref
+) => {
   return (
     <button
+      ref={ref}
       onClick={onClick}
       className={classNames("Button", type, icon && "ICON")}
     >
@@ -23,4 +22,4 @@ const Button = ({
   );
 };
 
-export default memo(Button);
+export default memo(forwardRef(Button));
--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./main.style.scss";
 import GroupLeaves from "../../containers/groupLeaves/groupLeaves";
 import SectionColors from "../../containers/sectionColors/SectionColors";
@@ -9,19 +9,56 @@ import SectionDressCode from "../../containers/sectionDressCode/sectionDressCode
 import Footer from "../../components/footer/footer";
 import Button from "../../components/button/button";
 import { BUTTON_TYPES } from "../../constants/general";
+import useIsMobile from "../../hooks/useIsMobile";
 
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-gsap.registerPlugin(ScrollToPlugin);
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 const MainPage = () => {
+  const isMobile = useIsMobile();
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    const buttonElem = buttonRef.current;
+    // console.log(buttonElem);
+    gsap.fromTo(
+      buttonElem,
+      {
+        // scale: 0,
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        // scale: 1,
+        opacity: 1,
+        duration: 0.5,
+        scrollTrigger: {
+          // trigger: "#colors",
+          trigger: buttonElem,
+          toggleClass: "locked",
+          toggleActions: "restart none none reverse",
+          start: () =>
+            `${
+              document.getElementById("colors").offsetTop +
+              (isMobile ? 76 : 92) -
+              document.getElementById("content").offsetHeight
+            }px top`,
+          end: () => `bottom ${window.innerHeight - (isMobile ? 16 : 32)}px`,
+          // pin: true,
+          // markers: true,
+        },
+      }
+    );
+  }, []);
   const handleGoTop = () => {
     gsap.to(window, { duration: 2, scrollTo: 0 });
   };
 
   return (
     <div className="MainPage">
-      <div className="MainPage__content">
+      <div id="content" className="MainPage__content">
         <GroupLeaves />
         <SectionCover />
         <SectionColors />
@@ -29,6 +66,7 @@ const MainPage = () => {
         <SectionDate />
         <SectionInfo />
         <Button
+          ref={buttonRef}
           type={BUTTON_TYPES.FAB}
           onClick={handleGoTop}
           icon="north"
