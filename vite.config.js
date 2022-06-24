89ad932b10873bd53b6527baa4d9d32e0cf38442
isaac-pj <isaac.brandaoe@osf.digital>
2022-06-24 00:49:21 -0300

    feat(fonts): add vite plugin fonts for load custom fonts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteFonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    ViteFonts({
+      google: {
+        families: ['Material+Symbols+Outlined'],
+        styles: 'opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+        preload: true,
+        defer: false,
+      },
+      custom: {
+        families: [
+          {
+            name: 'Integral CF',
+            local: 'Integral CF',
+            src: './src/assets/fonts/IntegralCF/IntegralCF-Regular.ttf',
+          },
+          {
+            name: 'Shadows Into Light',
+            local: 'Shadows Into Light',
+            src:
+              './src/assets/fonts/Shadows_Into_Light/ShadowsIntoLight-Regular.ttf',
+          },
+          {
+            name: 'Montserrat',
+            local: 'Montserrat',
+            src: './src/assets/fonts/Montserrat/static/Montserrat*',
+          },
+        ],
+        preload: true,
+        defer: false,
+      },
+    }),
+  ],
 })
